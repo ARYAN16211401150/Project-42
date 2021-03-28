17e4a96f4ebb75b951c37ec034617003d8838980
ARYAN16211401150 <74659123+ARYAN16211401150@users.noreply.github.com>
2021-03-28 11:18:06 +0530

    Add files via upload

---
+++ Drops.js
@@ -0,0 +1,25 @@
+class Drops
+{
+  constructor(x,y){
+    var options = {
+        friction: 0.001,
+        restitution:0.1           
+    }
+    this.rain = Bodies.circle(x,y,5,options)
+    this.r = 5;
+    World.add(world, this.rain);
+}
+    update1()
+    {
+      if(this.rain.position.y > height)
+      {
+        Matter.Body.setPosition(this.rain, {x:random(0,600), y:random(0,700)})
+      }
+    }
+    display()
+    {
+      ellipseMode(CENTER);
+      fill("blue")
+      ellipse(this.rain.position.x,this.rain.position.y,this.r,this.r)
+    }
+}
